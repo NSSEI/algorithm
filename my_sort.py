312770911622c76332db76aa74a886911e420f93
NSSEI <chatswoodj@gmail.com>
2021-04-22 16:45:03 +0900

    add quick_sort

--- my_sort.py
+++ my_sort.py
@@ -9,6 +9,31 @@ def insertion_sort(nums_list):
     return nums_list
 
 
+def quick_sort(nums_list, first, last):
+    if first < last:
+        split_point = partition(nums_list, first, last)
+        quick_sort(nums_list, first, split_point - 1)
+        quick_sort(nums_list, split_point + 1, last)
+    return nums_list
+
+def partition(nums_list, first, last):
+    pivot = nums_list[first]
+    left = first + 1
+    right = last
+    done = False
+    while not done:
+        while left <= right and nums_list[left] <= pivot:
+            left += 1
+        while left <= right and pivot <= nums_list[right]:
+            right -= 1
+        if right < left:
+            done = True
+        else:
+            nums_list[left], nums_list[right] = nums_list[right], nums_list[left]
+    nums_list[first], nums_list[right] = nums_list[right], nums_list[first]
+    return right
+
 
 if __name__ == "__main__":
-    print(insertion_sort([9,9,10,11,5,4,3,2,1,7,8]))
+    print(insertion_sort([9, 9, 10, 11, 5, 4, 3, 2, 1, 7, 8]))
+    print(quick_sort([9, 9, 10, 11, 5, 4, 3, 2, 1, 7, 8], 0, 10))
